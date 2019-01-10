09291d4a88f14a88f1daeb2ee9217ec0ebee67b9
mino.akihiro <mino.akihiro@trans-cosmos.co.jp>
2019-01-10 15:36:37 +0900

    update

--- serviceworker.js
+++ serviceworker.js
@@ -1,43 +1,11 @@
-//キャッシュ名（今回はmanekineko という固有の名前）
-var cacheName = 'manekineko';
-
-//キャッシュしたいファイルのリスト(今回は3種類。HTML本体、jsファイル、CSSファイルが対象)
-var filesToCache = [
-   'index.html',
-   'jquery.js',
-   'style.css',
-];
-
-//ブラウザへのインストール時にキャシュしたいファイルを追加する
-self.addEventListener('install',function(event){
-  event.waitUntil(
-    caches.open(cacheName).then(function(cache){
-      return cache.addAll(filesToCache);
-    })
-  );
+// service-worker.js
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
 });
 
-
-//ServiceWorkerが有効になるときcacheNameがちがうキャッシュを削除する
-self.addEventListener('activate',function(event){
-  event.waitUntil(
-    caches.keys().then(function(keyList){
-      return Promise.all(keyList.map(function(key){
-        if(key !== cacheName){
-          return caches.delete(key);
-        }
-      }));
-    })
-  );
-  return self.clients.claim();
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
 });
 
-
-//キャッシュがあれば呼び出し、ない場合ネットワークから取りに行く
-self.addEventListener('fetch',function(event){
-  event.respondWith(
-    caches.match(event.request).then(function(response){
-      return response || fetch(event.request);
-    })
-  );
-});
+// 現状では、この処理を書かないとService Workerが有効と判定されないようです
+self.addEventListener('fetch', function(event) {});
